3b3627777acea9c2609d6eaedea133aae1066642
Katrin <webentwicklerinKatrin@gmail.com>
2018-06-23 17:39:47 +0200

    ein paar dinge hinzugefuegt

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	//jumbotron carousel
+	$('.carousel').slick({
+	arrows: true,
+	autoplay: true,
+	autoplaySpeed: 4000,
+	slidesToScroll: 1,
+	pauseOnFocus: false,
+	pauseOnHover: false,
+	draggable: true
+	});
+
+});
